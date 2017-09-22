016b4cb433ea8d671021de643a2e173131c9ff91
Bradley Bauer <gazingatstars.o7@gmail.com>
2017-09-22 17:37:30 -0400

    Add functions to skeleton.

--- util.hpp
+++ util.hpp
@@ -1,4 +1,5 @@
 #include <eigen3/Eigen/Eigen>
+#include <png++/png.hpp>
 class Process {
 public:
 	Process(){};
@@ -14,6 +15,23 @@ public:
 		// compute zero padded convolution of data with [-1, 0, 1]
 		//    store convolution in temporary matrix
 		// copy temporary matrix to data
+	}
+
+	void intensity(png::image<png::rgb_pixel>& image, Eigen::MatrixXd& data) {
+		// set data_ij = length(image_ij)^2
+	}
+
+	void computeVertices(Eigen::Matrix<double, Dynamic, 2> vertices,
+	                     Eigen::MatrixXd& data) {
+    // if distribution(0,1) < data_ij
+    //     add vertex(i, j) to vertices
+	}
+
+	void colorImageWithVertices(png::image<png::rgb_pixel>& image,
+	                            Eigen::Matrix<double, Dynamic, 2> vertices) {
+		// loop through image pixels
+		//    find vertex that is closest to pixel
+		//    set pixel to average color of image around point vertex
 	}
 
 };
