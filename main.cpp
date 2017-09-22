ccaab79f38372c724b367cfabae3d2db6b518858
Bradley Bauer <gazingatstars.o7@gmail.com>
2017-09-22 16:59:33 -0400

    Add main skeleton.

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <png++/png.hpp>
+#include "util.hpp"
+int main() {
+	// Possibly accept arguments from user using gflags?
+
+	// Load image using png++
+	// Put image into eigen matrix
+
+	// Compute transformations
+
+	// Write image to output file
+	return 0;
+}
