46511c1b356c93b55948a06fa95be83afae7489b
Bradley Bauer <gazingatstars.o7@gmail.com>
2017-09-22 17:58:05 -0400

    Use eigen namespace.

--- util.hpp
+++ util.hpp
@@ -1,34 +1,39 @@
 #include <eigen3/Eigen/Eigen>
 #include <png++/png.hpp>
+using namespace Eigen;
 class Process {
 public:
 	Process(){};
 
-	void gaussianConvolve(int kernel_size, Eigen::MatrixXd& data) {
+	void gaussianConvolve(int kernel_size, MatrixXd& data) {
 		// compute gaussian kernel
 		// compute zero padded convolution of data with kernel
 		//    store convolution in temporary matrix
 		// copy temporary matrix to data
 	}
 
-	void gradient(Eigen::MatrixXd& data) {
+	void gradient(MatrixXd& data) {
 		// compute zero padded convolution of data with [-1, 0, 1]
 		//    store convolution in temporary matrix
 		// copy temporary matrix to data
 	}
 
-	void intensity(png::image<png::rgb_pixel>& image, Eigen::MatrixXd& data) {
+	void intensity(png::image<png::rgb_pixel>& image, MatrixXd& data) {
 		// set data_ij = length(image_ij)^2
+
+		// for (int i = 0; i < data_mat.cols(); ++i)
+		// 	for (int j = 0; j < data_mat.rows(); ++j)
+		// 		data_mat(j,i) = image[j][i];
 	}
 
-	void computeVertices(Eigen::Matrix<double, Dynamic, 2> vertices,
-	                     Eigen::MatrixXd& data) {
+	void computeVertices(Matrix<double, Dynamic, 2> vertices,
+	                     MatrixXd& data) {
     // if distribution(0,1) < data_ij
     //     add vertex(i, j) to vertices
 	}
 
 	void colorImageWithVertices(png::image<png::rgb_pixel>& image,
-	                            Eigen::Matrix<double, Dynamic, 2> vertices) {
+	                            Matrix<double, Dynamic, 2> vertices) {
 		// loop through image pixels
 		//    find vertex that is closest to pixel
 		//    set pixel to average color of image around point vertex
